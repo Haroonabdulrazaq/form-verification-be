0e184d9d666fc7eea9838577c023e46398375be4
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-02-10 15:21:28 +0100

    Successfully save users data

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,43 +25,49 @@ const createUser = (req, res) => {
 
   const errors = validationResult(req);
 
-  if(!errors.isEmpty()){ //if error is not empty, meaning there is an error while sanitizing user input above 
+  if(!errors.isEmpty()){ //  if error is not empty, meaning there is an error while sanitizing user input above 
     res.json({ msg: 'Error occured while creating new user, please check your inputs', error: errors.array() });
     return;
   } else {
-    pool.query(`SELECT * FROM myusers WHERE email=${req.body.email.toLowerCase()}`, (error, result)=>{
+    // console.log('Lowercase email', req.body.email.toLowerCase()); ${req.body.email};
+    pool.query('SELECT * FROM myusers WHERE email = $1', [req.body.email], (error, result)=>{
       if(error) {
-        res.json({msg: 'Oops an error occured, while ', error});
+        res.json({ msg: 'Oops an error occured, while checking if user exist', error });
         return;
       }
-      if(result) {
-        res.json({msg: 'Email is in use by another user', error});
+      if(result.length > 0) {
+        console.log('Searching if user exist condition', result);
+        res.json({ msg: 'Email is in use by another user', result: result.rows });
         return;
       } else {
-        pool.query(insert, (error, result)=>{
+        const {email, firstname, lastname, password} = req.body;  // Destructuring from req.body
+       // const insertCommand = 'INSERT INTO myusers (email, firstname, lastname, password) VALUES ($1, $2, $3, $4)', [email, firstname, lastname, password]
+        
+        pool.query('INSERT INTO myusers (email, firstname, lastname, password) VALUES ($1, $2, $3, $4)',
+          [email, firstname, lastname, password],
+          (error, result)=>{
           if(error) {
             res.json({ msg: 'Error occured while creating new user', error: error });
             return;
           }
-          console.log('');
-          res.json({ status: 200, msg: 'User is saved sucessfully', result: result.rows });
+          console.log('Inserting condition');
+          res.json({ status: 200, msg: 'User is saved successfully', result: result.rows });
         })
       }
-
     })
   }
-
-
-  const {email, firstname, lastname, password } = req.body //Destructuring email and firstname from req.body
-
-  pool.query('INSERT INTO myusers (email, firstname, lastname, password) VALUES ($1, $2, $3, $4)', [email, firstname, lastname, password], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    console.log('I am the result', results);
-    res.status(201).json(`User added with ID: ${results.rows}`);
-  })
 }
+
+// const createUser = (req, res)=> {
+//   const {email, firstname, lastname, password } = req.body //Destructuring email and firstname from req.body
+//   pool.query('INSERT INTO myusers (email, firstname, lastname, password) VALUES ($1, $2, $3, $4)', [email, firstname, lastname, password], (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     console.log('I am the result', results);
+//     res.status(201).json(`User added with ID: ${results.rows}`);
+//   })
+// }
 
 // const createUser = [
 //     body("firstname")
