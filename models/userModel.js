7881dc11d8eb3245f1826c4ff7d28710792d6821
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-02-11 21:38:12 +0100

    Preping for deployment

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,12 @@ const { Pool } = pkg;
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production" // checking Environment
+
+//connenction string
+const connectionString = `postgresql://${process.env.process.env.user}:${process.env.password}@${process.env.host}:${process.env.port}/${process.env.database}`
+
+
 const pool = new Pool({
   user: process.env.user,
   host: process.env.host,
