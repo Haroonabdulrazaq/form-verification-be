2790f0da5c7f33cd05d2d5ede31e605571591439
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-02-11 22:26:10 +0100

    Add procfile

--- models/userModel.js
+++ models/userModel.js
@@ -7,15 +7,14 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV === "production" // checking Environment
 
 //connenction string
-const connectionString = `postgresql://${process.env.process.env.user}:${process.env.password}@${process.env.host}:${process.env.port}/${process.env.database}`
+const connectionString = `postgresql://${process.env.user}:${process.env.password}@${process.env.host}:${process.env.port}/${process.env.database}`
 
 
 const pool = new Pool({
-  user: process.env.user,
-  host: process.env.host,
-  database: process.env.database,
-  password: '',
-  port: process.env.port,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export default pool;
